05f5fc4a73a772216ec2848352443546e414a500
cmmarone <cmmarone@gmail.com>
2021-05-03 14:32:40 -0400

    added Edit functionality

--- RestaurantRater2/Controllers/RestaurantController.cs
+++ RestaurantRater2/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater2.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -56,7 +57,7 @@ namespace RestaurantRater2.Controllers
             return View(restaurant);
         }
 
-        // POST Restaurant/Delete/{id}
+        // POST: Restaurant/Delete/{id}
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
@@ -65,6 +66,40 @@ namespace RestaurantRater2.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET: Restaurant/Edit/{id}
+        // Get and id from the user
+        // Handle if the id is null
+        // Find a Restaurant by that id
+        // Handle If the restaurant doesn't exist
+        // Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
